0e5c8acc2ed6b9aaf845893864dc0ea4a800eca8
Phatcharphat <n.phatcharphat@gmail.com>
2025-04-25 15:54:37 +0700

    update test case

---
+++ cypress/e2e/qaquiz.cy.js
@@ -0,0 +1,28 @@
+import { locators } from "../locator/locator";
+import 'cypress-real-events/support';
+
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    if (err.message.includes('TVC_GTM_Enhanced is not defined') || err.message.includes('Cannot read properties of undefined (reading \'map\')')) {
+      return false;
+    }
+    return true;
+  });
+  
+  describe('QA Quiz Automated Testing', () => {
+    it('Test Case No. 01: search and submit button before add to cart', () => {
+      cy.visit('https://www.bewellstyle.com');
+      cy.get(locators.homePage.cookiesButton).click();
+      // cy.get(locators.homePage.searchButton).click();
+      // cy.get(locators.homePage.searchField).type('เบาะรองหลังเพื่อสุขภาพ สำหรับรถยนต์ | Car Seat Back Cushion');
+      // cy.get(locators.homePage.searchSubmit).click();
+      // cy.get('.post-35797').realHover().wait(500);
+      // cy.get('.post-35797 .btn-add a').should('be.visible').scrollIntoView().click({ force: true });
+      // cy.wait(5000);
+      cy.get(locators.homePage.searchButton).click();
+      cy.get(locators.homePage.searchField).type('เบาะรองหลังทรงสูง | Healthy Back');
+      cy.get('#pa_color').select('สีน้ำเงิน - เบาะรองหลังทรงสูง');
+      cy.get('.single_add_to_cart_button').click();
+    });
+  });
+  
---
+++ cypress/locator/locator.js
@@ -0,0 +1,14 @@
+export const locators = {
+    homePage: {
+      cookiesButton: '#dpdpa--popup-accept-all',
+      searchButton: '.wrapp-header > .right-column > .search-button > a > .fa',
+      searchField: '.wrapp-header > .right-column > .search-button > .basel-search-wrapper > .basel-search-inner > #searchform > div > #s',
+      searchSubmit: '.wrapp-header > .right-column > .search-button > .basel-search-wrapper > .basel-search-inner > #searchform > div > #searchsubmit',
+    },
+    productCard: {
+      name: '.product-card .name',
+      price: '.product-card .price',
+      addToCartBtn: '.product-card .add-to-cart',
+    },
+  };
+  
